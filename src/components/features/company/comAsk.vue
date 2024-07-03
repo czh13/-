4cbe8ef87fd1f0307a5b507c34c7b08255225e05
milton <caizhihao.milton@gmail.com>
2024-07-03 16:57:18 +0800

    init

--- src/components/features/company/comAsk.vue
+++ src/components/features/company/comAsk.vue
@@ -4,20 +4,16 @@
 			<div class="flex flex-col items-center gap-6">
 				<ScTitle>Get in touch with Toppay</ScTitle>
 				<h4 class="font-semibold text-9 self-start tp-t-color">Interested in learning what we can do for your business?</h4>
-				<div class="flex items-start">
-					<div class="self-start flex flex-col">
-						<h4 class="text-10 mb-4">Our team can help you:</h4>
-						<div class="flex flex-col gap-5 mb-30">
-							<div class="flex items-start gap-4" v-for="help in helpTextList" :key="help">
-								<div class="w-5.5 h-5.5 tp-bg-color rounded-[50%] mt-2"></div>
-								<p class="flex-1 text-5.5 leading-9 font-medium">{{ help }}</p>
-							</div>
+				<div class="self-start flex flex-col">
+					<h4 class="text-10 mb-4">Our team can help you:</h4>
+					<div class="flex flex-col gap-5 mb-30">
+						<div class="flex items-start gap-7.5" v-for="help in helpTextList" :key="help">
+							<div class="w-5.5 h-5.5 tp-bg-color rounded-[50%] mt-2"></div>
+							<p class="flex-1 text-6.5 leading-9 font-medium">{{ help }}</p>
 						</div>
 					</div>
-					<div class="shadow-[0px_4px_40px_0px_#0961CF1A] px-17.5 py-12.5 bg-[#fff] rounded-3">
-						<ComForm />
-					</div>
 				</div>
+				<BaseColorButton :px="10" :py="3" :size="5" bgColor="#0961CF" color="#fff" @click="handleGoEmail">Ask for more info</BaseColorButton>
 			</div>
 		</div>
 	</section>
